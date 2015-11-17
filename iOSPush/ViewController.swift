f75ee93dfce23100c5ee6aeddfd32943ea29c6c0
Siva Ganesh <mail@sivaganesh.com>
2015-11-17 16:16:58 +0530

    init version

---
+++ iOSPush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSPush
+//
+//  Created by Siva Ganesh on 17/11/15.
+//  Copyright © 2015 Siva Ganesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOSPushTests/iOSPushTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSPushTests.swift
+//  iOSPushTests
+//
+//  Created by Siva Ganesh on 17/11/15.
+//  Copyright © 2015 Siva Ganesh. All rights reserved.
+//
+
+import XCTest
+@testable import iOSPush
+
+class iOSPushTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
